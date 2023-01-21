eee1cc3c1051c7a5293e39720f9d1ef9c8afb44c
imhalid <imhalid@icloud.com>
2023-01-22 00:01:33 +0300

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import CodeMirror from '@uiw/react-codemirror';
-import { javascript } from '@codemirror/lang-javascript';
-import ConsoleLog from '@/utils/console';
-import { useState } from 'react';
-import Script from 'next/script';
+import React from 'react'
+import CodeMirror from '@uiw/react-codemirror'
+import { javascript } from '@codemirror/lang-javascript'
+import ConsoleLog from '@/utils/console'
+import { useState } from 'react'
 
 function Home() {
-  const [value, setValue] = useState(console.log);
-  
+  const [value, setValue] = useState(console.log)
+
   const onChange = React.useCallback((data, viewUpdate) => {
-    setValue(data);
-  }, []);
+    setValue(data)
+  }, [])
   return (
     <div>
-    <CodeMirror
-      height="200px"
-      extensions={[javascript({ jsx: true })]}
-      onChange={onChange}
+      <CodeMirror
+        height='200px'
+        extensions={[javascript({ jsx: true })]}
+        onChange={onChange}
       />
       <button onClick={() => new Function(value)()}>Click me</button>
-    <ConsoleLog />
-      </div>
-  );
+      <ConsoleLog />
+    </div>
+  )
 }
-export default Home;
+export default Home
